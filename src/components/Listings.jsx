3b396d99185263d48ace4b0dbb53cf9a3734f043
maheshkarbhardev <168530791+maheshkarbhardev@users.noreply.github.com>
2024-07-10 10:10:44 +0530

    Update Listings.jsx

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -18,8 +18,8 @@ const Listings = () => {
     try {
       const response = await fetch(
         selectedCategory !== "All"
-          ? `http://localhost:3001/properties?category=${selectedCategory}`
-          : "http://localhost:3001/properties",
+          ? `https://hotelrentmernbackend.onrender.com/properties?category=${selectedCategory}`
+          : "https://hotelrentmernbackend.onrender.com/properties",
         {
           method: "GET",
         }
