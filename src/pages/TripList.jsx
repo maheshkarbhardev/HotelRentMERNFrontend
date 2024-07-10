cc53a9c649a7020647df14446a279aa357a9826d
maheshkarbhardev <168530791+maheshkarbhardev@users.noreply.github.com>
2024-07-10 10:23:55 +0530

    Update TripList.jsx

--- src/pages/TripList.jsx
+++ src/pages/TripList.jsx
@@ -16,7 +16,7 @@ const TripList = () => {
   const getTripList = async () => {
     try {
       const response = await fetch(
-        `http://localhost:3001/users/${userId}/trips`,
+        `https://hotelrentmernbackend.onrender.com/users/${userId}/trips`,
         {
           method: "GET",
         }
